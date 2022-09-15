1414191a179c6079d6b5fd38e543cf71247fc1f1
findfreddy <63041123+findfreddy@users.noreply.github.com>
2022-09-15 17:01:45 +0700

    BACK TO WORKING MODEL
    -still at static / now moving to Modules

--- controllers/search.js
+++ controllers/search.js
@@ -5,5 +5,7 @@ const router = express.Router()
 // Views
 // Create here a controller that accepts GET requests and renders the "search" page
 
+router.get('/', (req, res) => app.render())
+
 // Export
 module.exports = router
